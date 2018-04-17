9c0474bc23a4b1d23f765a0b93f6e56879be7be4
ChloeCqt <chloecaquant@hotmail.com>
2018-04-17 10:36:17 +0200

    Affichage event

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
     <ul>
-        @foreach($event as $e)
-            <li><a href="/event/{{$e->id}}">{{$e->text}}</a></li>
-        @endforeach
+            <li>{{$event->text}}</li>
+
     </ul>
 
 @endsection
